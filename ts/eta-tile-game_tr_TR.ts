254e740077b3cbc67822c7f7a6cfa5003d717fb7
Fatih Altun <fatih.altun@pardus.org.tr>
2018-12-11 14:18:08 +0000

    Update translation
    Signed-off-by: Fatih Altun <fatih.altun@pardus.org.tr>

--- ts/eta-tile-game_tr_TR.ts
+++ ts/eta-tile-game_tr_TR.ts
@@ -4,59 +4,84 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="qml/main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>ETA Tile</source>
-        <translation>ETA Fayans</translation>
+        <translation>Eta Fayans</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="41"/>
+        <location filename="../qml/main.qml" line="41"/>
         <source>TILE</source>
         <translation>FAYANS</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="51"/>
+        <location filename="../qml/main.qml" line="51"/>
         <source>GAME</source>
         <translation>OYUNU</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="71"/>
+        <location filename="../qml/main.qml" line="71"/>
         <source>&lt;b&gt;SCORE&lt;/b&gt;</source>
-        <translation>PUAN</translation>
+        <translation>&lt;b&gt;PUAN&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="71"/>
+        <location filename="../qml/main.qml" line="71"/>
         <source>&lt;b&gt;BEST&lt;/b&gt;</source>
-        <translation>EN İYİ PUAN</translation>
+        <translation>&lt;b&gt;EN İYİ PUAN&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="131"/>
+        <location filename="../qml/main.qml" line="131"/>
         <source>&lt;b&gt;Join the numbers and get to the 2048 tile!&lt;/b&gt;</source>
         <translation>&lt;b&gt;Sayıları birleştir ve 2048 sayısını elde et !&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="148"/>
+        <location filename="../qml/main.qml" line="148"/>
         <source>New Game</source>
         <translation>Yeni Oyun</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="227"/>
+        <location filename="../qml/main.qml" line="228"/>
         <source>Game Over</source>
         <translation>Oyun Bitti</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="228"/>
+        <location filename="../qml/main.qml" line="234"/>
         <source>Game Over!</source>
-        <translation>Oyun Bitti!</translation>
+        <translation>Kaybettiniz!</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="238"/>
+        <location filename="../qml/main.qml" line="253"/>
+        <source>Retry</source>
+        <translation>Tekrar Dene</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="279"/>
+        <source>Exit</source>
+        <translation>Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="297"/>
         <source>You Win</source>
         <translation>Kazandınız</translation>
     </message>
     <message>
-        <location filename="qml/main.qml" line="239"/>
-        <source>You win! Continue playing?</source>
-        <translation>Kazandınız! Kaldığınız yerden devam etmek ister misiniz ?</translation>
+        <location filename="../qml/main.qml" line="308"/>
+        <source>You win!</source>
+        <translation>Kazandınız!</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="316"/>
+        <source>Continue playing?</source>
+        <translation>Devam Etmek İster misiniz?</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="335"/>
+        <source>Yes</source>
+        <translation>Evet</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="360"/>
+        <source>No</source>
+        <translation>Hayır</translation>
     </message>
 </context>
 </TS>
