2bc07e70ce2c24cc34d179f30e80f4eef7279486
Fatih Altun <fatih.altun@pardus.org.tr>
2018-12-17 11:25:36 +0000

    Update translation
    Signed-off-by: Fatih Altun <fatih.altun@pardus.org.tr>

--- translations/eta-tile-game.ts
+++ translations/eta-tile-game.ts
@@ -1,87 +1,87 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="tr_TR">
+<TS version="2.1">
 <context>
     <name>main</name>
     <message>
         <location filename="../ui/main.qml" line="16"/>
         <source>ETA Tile Game</source>
-        <translation>ETA Fayans Oyunu</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="45"/>
         <source>TILE</source>
-        <translation>FAYANS</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="55"/>
         <source>GAME</source>
-        <translation>OYUNU</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="75"/>
         <source>&lt;b&gt;SCORE&lt;/b&gt;</source>
-        <translation>&lt;b&gt;PUAN&lt;/b&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="75"/>
         <source>&lt;b&gt;BEST&lt;/b&gt;</source>
-        <translation>&lt;b&gt;EN İYİ PUAN&lt;/b&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="135"/>
         <source>&lt;b&gt;Join the numbers and get to the 2048 tile!&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Sayıları birleştir ve 2048 sayısını elde et !&lt;/b&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="152"/>
         <source>New Game</source>
-        <translation>Yeni Oyun</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="232"/>
         <source>Game Over</source>
-        <translation>Oyun Bitti</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="238"/>
         <source>Game Over!</source>
-        <translation>Kaybettiniz!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="257"/>
         <source>Retry</source>
-        <translation>Tekrar Dene</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="282"/>
         <source>Exit</source>
-        <translation>Çıkış</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="300"/>
         <source>You Win</source>
-        <translation>Kazandınız</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="311"/>
         <source>You win!</source>
-        <translation>Kazandınız!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="319"/>
         <source>Continue playing?</source>
-        <translation>Devam Etmek İster misiniz?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="338"/>
         <source>Yes</source>
-        <translation>Evet</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/main.qml" line="363"/>
         <source>No</source>
-        <translation>Hayır</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
